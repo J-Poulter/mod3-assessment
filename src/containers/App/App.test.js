4c0cc43748316f784abd7e7df55257009be98012
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-22 11:39:52 -0600

    Create tests for App and ensure they are passing correctly

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,7 +1,49 @@
 import App from './App';
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
+
+const testStore = createStore(rootReducer)
+
+const mockOrders = [
+  { id: 1, name: 'Pat', ingredients: ['beans', 'cheese', 'lettuce'] },
+  { id: 2, name: 'Patrick', ingredients: ['carnitas', 'queso fresco', 'lettuce'] }
+]
+
+const newOrder = { id: 3, name: 'Frank', ingredients: ['steak', 'guacamole', 'jalapenos'] }
+
+const renderTestWrapper = () => {
+  return render(
+    <Provider store={testStore}>
+      <App />
+    </Provider>
+  )
+}
+
 
 describe('App', () => {
-  it('should', () => {
+  it('should render the correct content', () => {
+    const { getByText, getAllByRole } = renderTestWrapper();
+    
+    const ingredientsButtons = getAllByRole('button');
+    const appTitle = getByText('Burrito Builder');
+    const noOrdersNotice = getByText('No orders yet!');
+    const submitButton = getByText('Submit Order');
+    const sofritasButton = getByText('sofritas');
+    const guacamoleButton = getByText('guacamole');
+    const steakButton = getByText('steak');
+    const noIngredientsNotice = getByText('Order: Nothing selected');
 
+    expect(ingredientsButtons).toHaveLength(13);
+    expect(appTitle).toBeInTheDocument();
+    expect(noOrdersNotice).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+    expect(sofritasButton).toBeInTheDocument();
+    expect(guacamoleButton).toBeInTheDocument();
+    expect(steakButton).toBeInTheDocument();
+    expect(noIngredientsNotice).toBeInTheDocument();
   })
 })
