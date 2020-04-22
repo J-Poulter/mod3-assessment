5745a2235b634da3f81197f268328d07cda7dfa9
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-22 11:39:36 -0600

    Create function that handles the user clicking on a delete order button

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Orders.css';
 import { connect } from 'react-redux';
-import { getOrders } from '../../apiCalls';
+import { getOrders, deleteOrder } from '../../apiCalls';
 import { setOrders } from '../../actions';
 
 class Orders extends Component {
@@ -12,10 +12,17 @@ class Orders extends Component {
       .catch(err => console.error('Error fetching:', err));
   }
 
+  handleOrderDelete = (orderId) => {
+    deleteOrder(orderId)
+    const updatedOrders = this.props.orders.filter(order => order.id !== orderId)
+    this.props.setOrders(updatedOrders)
+  }
+
   orderEls = () => { 
     return this.props.orders.map(order => {
       return (
         <div className="order" key={order.id}>
+          <button onClick={() => this.handleOrderDelete(order.id)}>Delete Order</button>
           <h3>{order.name}</h3>
           <ul className="ingredient-list">
             {order.ingredients.map(ingredient => {
